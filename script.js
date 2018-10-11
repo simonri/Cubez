522a6cb10c66fb6c97d8424fa092001acb5ac6f5
Simon Risberg <simon.a.risberg@gmail.com>
2018-10-11 22:45:06 +0200

    Changed from "function" to "class" type classes.

--- script.js
+++ script.js
@@ -1,107 +1,105 @@
 'use strict';
-var ctx = document.getElementById("c").getContext("2d");
+const ctx = document.getElementById("c").getContext("2d");
 
-var textures = [
-    document.getElementsByTagName("img")[1],
-    document.getElementsByTagName("img")[0],
-    document.getElementsByTagName("img")[2],
-    document.getElementsByTagName("img")[3],
-    document.getElementsByTagName("img")[4],
-];
+const textures = document.getElementsByTagName("img");
 
-function Tile(cX, cY, w, h, texture) {
-    this.cX = cX;
-    this.cY = cY;
+class Tile {
+    constructor(cX, cY, w, h, texture) {
+        this.cX = cX;
+        this.cY = cY;
 
-    this.x = 0;
-    this.y = 0;
+        this.x = 0;
+        this.y = 0;
 
-    this.w = w;
-    this.h = h;
+        this.w = w;
+        this.h = h;
 
-    this.texture = texture;
+        this.texture = texture;
+    }
 
-    this.render = function() {
+    render(devMode) {
         if(this.texture === 3) {
             ctx.drawImage(textures[1], 0, 0, 256, 512, this.x, this.y, this.w, this.h);
-            ctx.drawImage(textures[1], 0, 512 / 2, 256, 512 / 2, this.cX * (this.w / 2), this.cY * (this.h / 4), this.w / 2, this.h / 4);
+            if(devMode)
+                ctx.drawImage(textures[1], 256/2 - 27, (512/4)*3+3, 54, 54, this.cX * (this.w / 2), this.cY * (this.h / 4), this.w / 2, this.h / 4);
         }
         ctx.drawImage(textures[this.texture], 0, 0, 256, 512, this.x, this.y, this.w, this.h);
-        ctx.drawImage(textures[this.texture], 0, 512 / 2, 256, 512 / 2, this.cX * (this.w / 2), this.cY * (this.h / 4), this.w / 2, this.h / 4);
+        if(devMode) // ctx.drawImage(textures[this.texture], 0, 512 / 2, 256, 512 / 2, this.cX * (this.w / 2), this.cY * (this.h / 4), this.w / 2, this.h / 4);
+            ctx.drawImage(textures[this.texture], 256/2 - 27, (512/4)*3+3, 54, 54, this.cX * (this.w / 2), this.cY * (this.h / 4), this.w / 2, this.h / 4);
 
-    };
+    }
 
-    this.update = function(x, y) {
+    update(x, y) {
         this.x = x;
         this.y = y;
-    };
+    }
 }
 
-function Game() {
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
+class Game {
+    constructor() {
+        ctx.canvas.width = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
 
-    let mouse = { x: 0, y: 0 };
+        this.mouse = { x: 0, y: 0 };
 
-    let worldSize = 900;
-    let offset = worldSize * 0.105;
+        this.worldSize = 900;
+        this.offset = this.worldSize * 0.105;
 
-    let offsetX = 0,
-        offsetY = 0;
+        this.offsetX = 0;
+        this.offsetY = 0;
 
-    let data = [
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,1,1,0,0,0],
-        [0,0,0,1,1,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-    ];
+        this.data = [
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,1,1,0,0,0],
+            [0,0,0,1,1,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+        ];
+        this.tiles = [];
 
-    let tileWidth = worldSize / data.length,
-        tileHeight = (worldSize / data.length) * (512 / 256);
+        this.tileWidth = this.worldSize / this.data.length;
+        this.tileHeight = (this.worldSize / this.data.length) * (512 / 256);
 
-    let times = [];
-    let tiles = [];
+        this.times = [];
 
-    /*let tiles = [].concat.apply([], data.map((x, i) => {
-      return x.map((y, j) => {
-          return new Tile(i, j, tileWidth, tileHeight, textures[y]);
-      });
-    }));*/
+        for(let i = 0; i < this.data.length; i++) {
+            for(let j = this.data.length - 1; j >= 0; j--) {
+                this.tiles.push(new Tile(i, j, this.tileWidth, this.tileHeight, this.data[i][j]));
+            }
+        }
 
+        this.devMode = false;
+    }
 
-    for(var i = 0; i < data.length; i++) {
-        for(var j = data.length - 1; j >= 0; j--) {
-            tiles.push(new Tile(i, j, tileWidth, tileHeight, data[i][j]));
+    clickTile(e) {
+        if(this.devMode) {
+            for(let i = 0; i < this.tiles.length; i++) {
+                let t = this.tiles[i];
+
+                let x = t.cX * (t.w / 2);
+                let y = t.cY * (t.h / 4);
+
+                if (e.clientX > x && e.clientX < x + t.w / 2) {
+                    if ((e.clientY > y && e.clientY < y + t.h / 4)) {
+                        this.data[t.cY][t.cX] = (t.texture < textures.length - 1) ? t.texture + 1 : 0;
+                        this.tiles[i].texture = (t.texture < textures.length - 1) ? t.texture + 1 : 0;
+                    }
+                }
+            }
         }
     }
 
-    document.onmousedown = function(e) {
-      for(i = 0; i < tiles.length; i++) {
-            let x = tiles[i].cX * (tiles[i].w / 2);
-            let y = tiles[i].cY * (tiles[i].h / 4);
-            if(
-                (e.clientX > x && e.clientX < x + tiles[i].w / 2) &&
-                (e.clientY > y && e.clientY < y + tiles[i].h / 4)
-            ) {
-                data[tiles[i].cY][tiles[i].cX] = (tiles[i].texture < textures.length - 1) ? tiles[i].texture + 1 : 0;
-                tiles[i].texture = (tiles[i].texture < textures.length - 1) ? tiles[i].texture + 1 : 0;
-            }
-        }
-        document.onmousemove = function(e) {
-            mouse.x += e.movementX;
-            mouse.y += e.movementY;
-        };
-    };
+    mouseMoved(e) {
+        this.mouse.x += e.movementX;
+        this.mouse.y += e.movementY;
+    }
 
-    document.onmouseup = function() { document.onmousemove = null };
-    
-    this.saveData = function() {
+    saveData() {
         let a = document.createElement("a");
-        let url = URL.createObjectURL(new Blob([data.join("\n")], {type: "txt"}));
+        let url = URL.createObjectURL(new Blob([this.data.join("\n")], {type: "txt"}));
         
         a.href = url;
         a.download = "data.txt";
@@ -112,43 +110,46 @@ function Game() {
         window.URL.revokeObjectURL(url);
     };
 
-    this.loop = function() {
+    loop() {
         this.render();
-        document.getElementById("fps").innerHTML = times.length + "- " + Math.floor((performance.memory.usedJSHeapSize / performance.memory.totalJSHeapSize) * 100) + "%";
+        document.getElementById("fps").innerHTML = this.times.length + " - " + Math.floor((performance.memory.usedJSHeapSize / performance.memory.totalJSHeapSize) * 100) + "%";
 
         window.requestAnimationFrame(() => {
             let now = performance.now();
-            while(times.length > 0 && times[0] <= now - 1000) {
-                times.shift();
+            while(this.times.length > 0 && this.times[0] <= now - 1000) {
+                this.times.shift();
             }
-            times.push(now);
-
+            this.times.push(now);
             this.loop();
         });
     };
 
-    this.render = function() {
+    render() {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.width);
 
-        offsetX += ((mouse.x * 0.5 - offsetX) * 0.1);
-        offsetY += ((mouse.y * 0.5 - offsetY) * 0.1);
+        this.offsetX += ((this.mouse.x * 0.5 - this.offsetX) * 0.1);
+        this.offsetY += ((this.mouse.y * 0.5 - this.offsetY) * 0.1);
 
         ctx.fillStyle = "lightgreen";
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-        for(var i = tiles.length - 1; i >= 0; i--) {
-            let t = tiles[i];
+        for(var i = this.tiles.length - 1; i >= 0; i--) {
+            let t = this.tiles[i];
 
-            let x = t.cX * t.w / 2 + t.cY * t.w / 2 + ctx.canvas.width / 2 + offsetX - (data.length * t.w / 2 + data.length * t.w / 2) / 2;
-            let y = t.cY / 2 * (t.h - offset) / 2 - t.cX / 2 * (t.h - offset) / 2 + ctx.canvas.height / 2 + 60 + offsetY - data.length / 2 * (t.h - offset) / 2;
+            let x = t.cX * t.w / 2 + t.cY * t.w / 2 + ctx.canvas.width / 2 + this.offsetX - (this.data.length * t.w / 2 + this.data.length * t.w / 2) / 2;
+            let y = t.cY / 2 * (t.h - this.offset) / 2 - t.cX / 2 * (t.h - this.offset) / 2 + ctx.canvas.height / 2 + 60 + this.offsetY - this.data.length / 2 * (t.h - this.offset) / 2;
 
-            tiles[i].update(x, y);
-            tiles[i].render();
+            this.tiles[i].update(x, y);
+            this.tiles[i].render(this.devMode);
         }
     };
 }
 
-var game = new Game();
+const game = new Game();
 game.loop();
 
-document.getElementById("save").onclick = function() { game.saveData() };
+document.getElementById("save").onclick = () => game.saveData();
+document.getElementById("dev").onclick = () => game.devMode = !game.devMode;
+
+document.onmousedown = e => { game.clickTile(e); document.onmousemove = e => game.mouseMoved(e); };
+document.onmouseup = () => document.onmousemove = null;

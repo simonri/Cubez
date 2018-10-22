8533f62f553a23ed47aa35c801ca2a0453227307
Simon Risberg <simon.a.risberg@gmail.com>
2018-10-22 11:17:41 +0200

    Idk

--- script.js
+++ script.js
@@ -250,4 +250,11 @@ class CVS {
     window.requestAnimationFrame = requestAnimationFrame;
     
     let cvs = new CVS();
+    
+    const express = require("express");
+    const router = express.Router();
+    router.get("/", (req, res) => {
+      res.send({ response: "I am alive" }).status(200);
+    });
+    module.exports = router;
 })();
